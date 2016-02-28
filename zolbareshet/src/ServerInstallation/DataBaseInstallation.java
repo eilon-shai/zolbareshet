6247fe5b9131d8cb5bf9a1095c86259f45303c76
shayperetz <shayperetz8@gmail.com>
2016-02-28 18:42:18 +0200

    adding JDBC postageSQL Driver + adding new connection

---
+++ zolbareshet/src/ServerInstallation/DataBaseInstallation.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ServerInstallation;
+
+import java.sql.*;
+/**
+ *
+ * @author peretzs
+ */
+public class DataBaseInstallation {
+  
+    
+    // final static String DriverClassName = 
+}
