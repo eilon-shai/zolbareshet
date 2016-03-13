d8985fa05f4d3dd90b7ed50a14dc6733e1d68456
shayperetz <shayperetz8@gmail.com>
2016-03-13 19:13:30 +0200

    adding classes 1. parts 2. user 3. phone 4. address

---
+++ zolbareshet/src/ProjectEntities/Parts.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjectEntities;
+
+/**
+ *
+ * @author peretzs
+ */
+public class Parts {
+    private int PartNumber ; 
+    private String PartName ; 
+    private String Description ; 
+    private int Price ; 
+    private int AvailableQuantity ; 
+    private boolean IsActive ; 
+    private int discount ; 
+    
+    public Parts(int partnumber, String partname , String description ,int Price , int availablequantity , boolean isactive , int discount )
+    {
+        
+    }
+    
+    //Part Number 
+    public int getPartNumber ()
+    {
+        return this.PartNumber;
+    }
+    
+    public void setPartNumber( int newNum )
+    {
+        this.PartNumber = newNum ; 
+    }
+    
+    
+    //Part Name 
+    
+    public String getPartName ()
+    {
+        return this.PartName;
+    }
+    
+    public void setPartName ( String partName )
+    {
+        this.PartName = partName ; 
+    }
+    
+    
+    //part description 
+    
+    public String getDescription ()
+    {
+        return this.Description ;
+    }
+    
+    public void setDescription (String NewDescription )
+    {
+         this.Description = NewDescription;
+    }
+    
+    //price 
+    
+    public int getPrice () 
+    {
+        return this.Price ; 
+    }
+    
+    public void setPrice (int NewPrice )
+    {
+        this.Price =NewPrice ; 
+    }
+    
+    //availablequantity
+    
+    public int getQuantity ()
+    {
+        return this.AvailableQuantity;
+    }
+    
+    public void setQuantity (int NewQuantity ) 
+    {
+        this.AvailableQuantity = NewQuantity ; 
+    }
+    
+    //Is Active 
+    
+    public boolean getIsActive ()
+    {
+        return this.IsActive;
+    }
+    
+    public void setIsActive (boolean isactive )
+    {
+        this.IsActive = isactive ; 
+    }
+    
+    //discount
+    
+    public int getDiscount ()
+    {
+        return this.discount;
+    }
+    
+    public void setDiscount(int newActive)
+    {
+        this.discount = newActive ; 
+    }
+}
+
